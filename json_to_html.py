e18e3a93a45b1a4ab0af7f942068078d0c7c5270
Kirill <belouskir@gmail.com>
2018-03-27 23:49:13 +0300

    3d task
    Решение 3й задачи с конвертацией списочных и словарных элементов входящего JSON.

--- json_to_html.py
+++ json_to_html.py
@@ -2,6 +2,9 @@ import json
 from collections import OrderedDict
 
 class Json2Html:
+    
+    converted = ""
+
     def load_file(self, json_file=""):
         """
             Loads JSON file
@@ -18,21 +21,41 @@ class Json2Html:
             raise SystemExit
 
 
+    def convert_list(self, json_data):
+        """
+            Convert json's list input data
+        """
+        converted_list = ''
+        for list_elem in list(map(self.convert_dict, json_data)):
+            converted_list += "<li>%s</li>" % list_elem
+        converted_list = '<ul>%s</ul>' % converted_list
+        return converted_list
+
+
+    def convert_dict(self, json_data):
+        """
+            Convert dict input data
+        """
+        converted_dict = ''
+        for elem_name, elem_attr in json_data.items():
+            converted_dict += '<%s>%s</%s>' % (elem_name, elem_attr, elem_name)
+        return converted_dict
+    
+
     def convert(self, json_input):
         """
             Convert JSON to HTML
         """
-        converted = ""
-        
-        for list_elem in json_input:
-            for elem_name, elem_attr in list_elem.items():
-                converted += '<%s>%s</%s>' % (elem_name, elem_attr, elem_name)
-        
+        if type(json_input) == list:
+            converted = self.convert_list(json_input)
+        else:
+            converted = self.convert_dict(json_input)
+
         return converted
 
 
 json2html = Json2Html()
-data = json2html.load_file(json_file='source2.json')
+data = json2html.load_file(json_file='source3.json')
 res = json2html.convert(data)
 print('=======Result==========')
 print(res)
--- test_json_to_html.py
+++ test_json_to_html.py
@@ -8,8 +8,8 @@ class TestJson2Html(unittest.TestCase):
         self.json2html = Json2Html()
     
     def test_task(self):
-        etalon_res = '<h3>Title #1</h3><div>Hello, World 1!</div>'
-        data = self.json2html.load_file(json_file='source2.json')
+        etalon_res = '<ul><li><h3>Title #1</h3><div>Hello, World 1!</div></li><li><h3>Title #2</h3><div>Hello, World 2!</div></li></ul>'
+        data = self.json2html.load_file(json_file='source3.json')
         res = self.json2html.convert(data)
         self.assertEqual(res, etalon_res)
 
