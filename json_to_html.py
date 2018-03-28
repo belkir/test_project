87ea7c210b7f8158b5ef17b99b2d1de82221cfe3
Kirill <belouskir@gmail.com>
2018-03-28 10:24:48 +0300

    5th task
    Решение 5й задачи. Использованы регулярные выражения для нахождения классов и id тэгов. Затем, также с помощью форматирования строк собираем полученный html.

--- json_to_html.py
+++ json_to_html.py
@@ -1,5 +1,7 @@
 import json
 from collections import OrderedDict
+import re
+from html import escape
 
 class Json2Html:
     
@@ -21,6 +23,24 @@ class Json2Html:
             raise SystemExit
 
 
+    def add_tag_attributes(self, elem_name):
+        """
+            RegExp func for finding id and class of tags
+        """
+        ids_list = re.findall(r'\#([a-zA-Z0-9-_]+)', elem_name)
+        css_classes = re.findall(r'\.([a-zA-Z0-9-_]+)', elem_name)
+        tag_name = re.findall(r'^([a-zA-Z0-9-_]+)', elem_name)[0]
+
+        if len(ids_list) > 0:
+            tag_attributes = 'id="%s" ' % ids_list[0]
+        else: 
+            tag_attributes = ''
+        if css_classes:
+            tag_attributes += 'class="%s"' % ' '.join(css_classes) 
+
+        return tag_name, tag_attributes
+
+
     def convert_list(self, json_data):
         """
             Convert json's list input data
@@ -38,11 +58,12 @@ class Json2Html:
         """
         converted_dict = ''
         for elem_name, elem_attr in json_data.items():
+            tag_name, tag_attributes = self.add_tag_attributes(elem_name)
             if type(elem_attr) == list:
                 converted_list = self.convert_list(elem_attr)
-                converted_dict += '<%s>%s</%s>' % (elem_name, converted_list, elem_name)
+                converted_dict += '<%s %s>%s</%s>' % (tag_name, tag_attributes, escape(converted_list), tag_name)
             else:
-                converted_dict += '<%s>%s</%s>' % (elem_name, elem_attr, elem_name)
+                converted_dict += '<%s %s>%s</%s>' % (tag_name, tag_attributes, escape(elem_attr), tag_name)
         return converted_dict
     
 
@@ -59,7 +80,7 @@ class Json2Html:
 
 
 json2html = Json2Html()
-data = json2html.load_file(json_file='source4_1.json')
+data = json2html.load_file(json_file='source5.json')
 res = json2html.convert(data)
 print('=======Result==========')
 print(res)
--- test_json_to_html.py
+++ test_json_to_html.py
@@ -8,19 +8,22 @@ class TestJson2Html(unittest.TestCase):
         self.json2html = Json2Html()
     
 
-    def test_task4_1(self):
-        etalon_res = '<ul><li><span>Title #1</span><content><ul><li><p>Example 1</p><header>header 1</header></li></ul></content></li><li><div>div 1</div></li></ul>'
-        data = self.json2html.load_file(json_file='source4_1.json')
+    def test_task5(self):
+        etalon_res = '<p id="my-id" class="my-class">hello</p><p class="my-class1 my-class2">example&lt;a&gt;asd&lt;/a&gt;</p>'
+        data = self.json2html.load_file(json_file='source5.json')
+        res = self.json2html.convert(data)
+        self.assertEqual(res, etalon_res)
+
+
+    def test_task5_extended(self):
+        etalon_res = '<p id="my-id" class="my-class">hello</p><p class="my-class1 my-class2">example&lt;a&gt;asd&lt;/a&gt;</p>'
+        data = json.loads("""{
+                "p #my-id .my-class": "hello",
+                "p .my-class1 .my-class2":"example<a>asd</a>"
+            }""")
         res = self.json2html.convert(data)
         self.assertEqual(res, etalon_res)
     
-
-    def test_task4_2(self):
-        etalon_res = '<p>hello1</p>'
-        data = self.json2html.load_file(json_file='source4_2.json')
-        res = self.json2html.convert(data)
-        self.assertEqual(res, etalon_res)
-
 
 if __name__ == '__main__':
     unittest.main()
